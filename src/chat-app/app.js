eec5451f1e520a66f4a92e2c3f838fbdd5314317
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-02-12 16:35:13 +0900

    Styling

--- src/chat-app/app.js
+++ src/chat-app/app.js
@@ -86,48 +86,68 @@ export default function($root) {
 
 function renderView($root, state, action) {
   render($root, () => html`
-    <h2>QuicChat</h2>
+    <h2 class="header">RTCQuicChat</h2>
     <section>
-      ${ state.isSignalingDone ? html`
-        <h3>Chat</h3>
-        <ul>
-          ${ state.messages.map(msg => html`
-          <li>${msg}</li>
-          `)}
-        </ul>
-        <textarea
-          value=${state.chatText}
-          oninput=${ev => action.$update('chatText', ev.target.value)}
-        ></textarea>
-        <button
-          type="button"
-          onclick=${action.sendText}
-        >Send</button>
-      ` : html`
-        <h3>Signaling</h3>
-        <div>
-          <h4>Params to send</h4>
-          <textarea
-            onclick=${ev => ev.target.select()}
-            oninput=${ev => action.$update('localParams', ev.target.value)}
-          >${state.localParams}</textarea>
-          <button
-            onclick=${action.getParams}
-            disabled=${state.iceRole !== ''}
-          >get()</button>
-        </div>
-        <div>
-          <h4>Params to receive</h4>
-          <textarea
-            onclick=${ev => ev.target.select()}
-            oninput=${ev => action.$update('remoteParams', ev.target.value)}
-          >${state.remoteParams}</textarea>
-          <button
-            onclick=${action.setParams}
-            disabled=${state.iceRole === 'controlled'}
-          >set()</button>
-        </div>
-      ` }
+      ${ state.isSignalingDone
+        ? renderChatView(state, action)
+        : renderSignalingView(state, action)
+      }
     </section>
   `);
+}
+
+function renderSignalingView(state, action) {
+  return html`
+    <h3 class="header">Step1. Signaling</h3>
+    <div>
+      <h4>Params to send</h4>
+      <textarea
+        onclick=${ev => ev.target.select()}
+        oninput=${ev => action.$update('localParams', ev.target.value)}
+        placeholder="Get() it OR paste it from remote peer."
+      >${state.localParams}</textarea>
+      <button
+        class="waves-effect waves-light btn"
+        onclick=${action.getParams}
+        disabled=${state.iceRole !== ''}
+      >get()</button>
+    </div>
+    <div>
+      <h4>Params to receive</h4>
+      <textarea
+        onclick=${ev => ev.target.select()}
+        oninput=${ev => action.$update('remoteParams', ev.target.value)}
+        placeholder="Paste it from remote peer, then set() it."
+      >${state.remoteParams}</textarea>
+      <button
+        class="waves-effect waves-light btn"
+        onclick=${action.setParams}
+        disabled=${state.iceRole === 'controlled'}
+      >set()</button>
+    </div>
+  `;
+}
+function renderChatView(state, action) {
+  return html`
+    <h3 class="header">Step2. Chat</h3>
+    <ul class="collection">
+      ${ state.messages.length === 0 ? html`
+      <li class="collection-item grey lighten-4">No messages</li>
+      ` : null }
+      ${ state.messages.map(msg => html`
+      <li class="collection-item">${msg}</li>
+      `)}
+    </ul>
+    <input
+      type="text"
+      value=${state.chatText}
+      oninput=${ev => action.$update('chatText', ev.target.value)}
+      placeholder="Enter a message..."
+    >
+    <button
+      class="waves-effect waves-light btn"
+      type="button"
+      onclick=${action.sendText}
+    >Send</button>
+  `;
 }
